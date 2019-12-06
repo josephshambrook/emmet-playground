fdbb150ce2d1f0bdf1b5f02a8e9342030afa3752
Joseph Shambrook <751062+josephshambrook@users.noreply.github.com>
2019-12-06 17:07:44 +0000

    Changed Example to buttons for accessibility

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 
-const Example = styled.div`
+const Example = styled.button`
   background-color: #282c34;
+  border: 0;
   border-radius: 10px;
   color: #fff;
   cursor: pointer;
   display: inline-block;
   font-family: "Courier New", Courier, monospace;
   font-size: 1rem;
+  line-height: 1.15;
   margin: 0 1rem 1rem 0;
+  overflow: visible;
   padding: 0.5rem 1rem;
+  text-transform: none;
+  -webkit-appearance: button;
 `;
 
 export default Example;
