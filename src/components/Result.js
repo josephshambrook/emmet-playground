b41fcb166f71093b6bc3c56c40850c5ea5202dba
Joseph Shambrook <751062+josephshambrook@users.noreply.github.com>
2020-06-12 09:41:01 +0100

    ....And again

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,16 @@
 import React, { useContext, useState, useEffect } from "react";
 import expand from "emmet";
 import styled from "styled-components";
-import Highlight from "react-highlight";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import vsDark from "prism-react-renderer/themes/vsDark";
 import { EmmetContext } from "./../hooks/useEmmet";
 
-const NoMargin = styled.div`
+const Container = styled.div`
   > pre {
+    border-radius: 5px;
     margin: 0;
+    overflow-x: auto;
+    padding: 1rem;
   }
 `;
 
@@ -30,14 +34,31 @@ const Result = () => {
   }, [emmet, emmetOptions]);
 
   return (
-    <NoMargin>
+    <Container>
       <h2>Output</h2>
       {invalid ? (
         <div>The text you've added is invalid!</div>
       ) : (
-        <Highlight language="html">{result}</Highlight>
+        <Highlight
+          {...defaultProps}
+          theme={vsDark}
+          code={result}
+          language="markup"
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
       )}
-    </NoMargin>
+    </Container>
   );
 };
 
